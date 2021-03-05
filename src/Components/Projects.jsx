39e329fc62148a72d113543a18af7123017dc69d
stephenPC <stephenmissah@outlook.com>
2021-03-05 19:10:46 +0000

    implemented slick - needs styling

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,33 +1,31 @@
 import React from "react";
 import ProjectDetails from "../data/projects-data.json";
-import AliceCarousel from "react-alice-carousel";
-import "react-alice-carousel/lib/alice-carousel.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import "../Component Styles/Projects/ProjectsPage.css";
+import Slider from "react-slick";
 
-const Projects = () => {
-  const handleDragStart = (e) => e.preventDefault();
-  const items = ProjectDetails.map(({ id, images: { mainImage } }) => {
-    return (
-      <img
-        key={id}
-        src={mainImage}
-        alt="project"
-        className="project__thumbnail"
-        onDragStart={handleDragStart}
-      />
-    );
-  });
-  console.log(items);
+export default function SimpleSlider() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
 
   return (
-    <div className="projects">
-      <AliceCarousel
-        mouseTracking
-        items={items}
-        className="projects__carusel"
-      />
-    </div>
+    <Slider {...settings}>
+      {ProjectDetails.map(({ id, images: { mainImage } }) => {
+        return (
+          <img
+            key={id}
+            src={mainImage}
+            alt="project"
+            className="project__thumbnail"
+          />
+        );
+      })}
+    </Slider>
   );
-};
-
-export default Projects;
+}
