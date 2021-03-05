cb8f47d24c562eed37a657207cf580f5908a4959
stephenPC <stephenmissah@outlook.com>
2021-03-05 17:49:06 +0000

    styling projects page using mapped json data

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import ProjectDetails from "../data/projects-data.json";
+import "../Component Styles/Projects/ProjectsPage.css";
 
 const Projects = () => {
-  return <div>projects</div>;
+  return (
+    <div className="projects container">
+      <div className="projects__images">
+        {ProjectDetails.map(
+          ({ id, title, description, images: { mainImage } }) => (
+            <div className="projects__imagesContainer">
+              <img
+                data-title={title}
+                key={id}
+                src={mainImage}
+                alt="main-image"
+                className="projects__imagesThumbnail"
+              />
+              <h2 className="project__title">{title}</h2>
+            </div>
+          )
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
--- src/Components/index.js
+++ src/Components/index.js
@@ -4,3 +4,4 @@ export { default as Home } from "./Home.jsx";
 export { default as Header } from "./Header.jsx";
 export { default as PageNotFound } from "./PageNotFound.jsx";
 export { default as Projects } from "./Projects.jsx";
+export { default as SingleProject } from "./SingleProject.jsx";
