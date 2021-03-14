13c3976f7ec84624fbd41446c334fb41b2dd1e69
stephenPC <stephenmissah@outlook.com>
2021-03-14 12:29:47 +0000

    need to added mobile q's

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,13 +1,15 @@
 import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 import ProjectSlider from "./ProjectSlider";
-import UseAnimations from "react-useanimations";
-import { BsBoxArrowInLeft } from "react-icons/bs";
+import { BsArrowBarLeft } from "react-icons/bs";
 import "../Component Styles/Projects/ProjectsPage.css";
 
 const Projects = () => {
   return (
     <div className="projects">
-      <BsBoxArrowInLeft className="projects__backButton" />
+      <Link to={"/"}>
+        <BsArrowBarLeft className="projects__backButton" />
+      </Link>
       <ProjectSlider />
     </div>
   );
--- src/Components/SingleProject.jsx
+++ src/Components/SingleProject.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import { BsArrowBarLeft } from "react-icons/bs";
+import { Link } from "react-router-dom";
 import projectsData from "../data/projects-data.json";
 import "../Component Styles/SingleProject/SingleProject.css";
 const SingleProject = ({ match }) => {
@@ -15,46 +17,52 @@ const SingleProject = ({ match }) => {
   const [{ description, id, images, skills, title } = {}] = singleProject;
   console.log(images);
   return (
-    <div className="project">
-      <div className="project__hero">
-        <h1>{title}</h1>
-        <img
-          src={
-            images
-              ? images.secondImage
-              : "https://designshack.net/wp-content/uploads/placeholder-image.png"
-          }
-          alt="project image"
-        />
-      </div>
+    <React.Fragment>
+      <Link to={"/projects/"}>
+        <BsArrowBarLeft className="projects__backButton" />
+      </Link>
 
-      <div className="project__images">
-        <img
-          src={
-            images
-              ? images.secondImage
-              : "https://designshack.net/wp-content/uploads/placeholder-image.png"
-          }
-          alt="project image"
-        />
-        <img
-          src={
-            images
-              ? images.secondImage
-              : "https://designshack.net/wp-content/uploads/placeholder-image.png"
-          }
-          alt="project image"
-        />
-        <img
-          src={
-            images
-              ? images.secondImage
-              : "https://designshack.net/wp-content/uploads/placeholder-image.png"
-          }
-          alt="project image"
-        />
+      <div className="project">
+        <div className="project__hero">
+          <h1>{title}</h1>
+          <img
+            src={
+              images
+                ? images.secondImage
+                : "https://designshack.net/wp-content/uploads/placeholder-image.png"
+            }
+            alt="project image"
+          />
+        </div>
+
+        <div className="project__images">
+          <img
+            src={
+              images
+                ? images.secondImage
+                : "https://designshack.net/wp-content/uploads/placeholder-image.png"
+            }
+            alt="project image"
+          />
+          <img
+            src={
+              images
+                ? images.secondImage
+                : "https://designshack.net/wp-content/uploads/placeholder-image.png"
+            }
+            alt="project image"
+          />
+          <img
+            src={
+              images
+                ? images.secondImage
+                : "https://designshack.net/wp-content/uploads/placeholder-image.png"
+            }
+            alt="project image"
+          />
+        </div>
       </div>
-    </div>
+    </React.Fragment>
   );
 };
 
