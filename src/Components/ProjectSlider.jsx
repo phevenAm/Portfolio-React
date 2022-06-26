72ff454f0d349bc43b8bb5aa962d3fea0407c048
Stephen Em <stephenmissah@outlook.com>
2022-06-26 15:16:34 +0100

    no swiper.js - moved to slickjs

--- src/Components/ProjectSlider.jsx
+++ src/Components/ProjectSlider.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import ProjectDetails from "../data/projects-data.json";
 // react-id-swiper
-import "swiper/css/swiper.css";
-import Swiper from "react-id-swiper";
+// import "swiper/css/swiper.css";
+// import Swiper from "react-id-swiper";
 //customs css
 import '../css/ProjectSlider.css';
 
@@ -27,38 +27,38 @@ const ProjectSlider = () =>
       el: ".swiper-pagination",
     },
   };
-  return (
-    <Swiper {...params}>
-      {ProjectDetails.map((project) =>
-      {
-        const {
-          id,
-          title,
-          description,
-          images: { mainImage },
-          skills,
-        } = project;
+  // return (
+  //   // <Swiper {...params}>
+  //   //   {ProjectDetails.map((project) =>
+  //   //   {
+  //   //     const {
+  //   //       id,
+  //   //       title,
+  //   //       description,
+  //   //       images: { mainImage },
+  //   //       skills,
+  //   //     } = project;
 
-        return (
-          <div
-            key={id}
-            className="project__slide"
-            onClick={() => console.log("use history APi to change page")}
-          >
-            <Link to={`/Projects/Single_Project/${title}`}>
-              <div className="project__slideElementContent">
-                <img src={mainImage} alt={title} />
-                <div className="project__slideElementContentText">
-                  <h1>{title}</h1>
-                  <p> {description}</p>
-                </div>
-              </div>
-            </Link>
-          </div>
-        );
-      })}
-    </Swiper>
-  );
+  //   //     return (
+  //   //       <div
+  //   //         key={id}
+  //   //         className="project__slide"
+  //   //         onClick={() => console.log("use history APi to change page")}
+  //   //       >
+  //   //         <Link to={`/Projects/Single_Project/${title}`}>
+  //   //           <div className="project__slideElementContent">
+  //   //             <img src={mainImage} alt={title} />
+  //   //             <div className="project__slideElementContentText">
+  //   //               <h1>{title}</h1>
+  //   //               <p> {description}</p>
+  //   //             </div>
+  //   //           </div>
+  //   //         </Link>
+  //   //       </div>
+  //   //     );
+  //   //   })}
+  //   // </Swiper>
+  // );
 };
 
 export default ProjectSlider;
