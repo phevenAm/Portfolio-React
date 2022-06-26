b59aeab48cea79b1e124f52e9c22d5a8687b3625
Stephen Em <stephenmissah@outlook.com>
2022-06-26 18:00:05 +0100

    noSlider branch
    still not working

--- src/Components/ProjectSlider.jsx
+++ src/Components/ProjectSlider.jsx
@@ -3,51 +3,48 @@ import { Link } from "react-router-dom";
 import ProjectDetails from "../data/projects-data.json";
 import Slider from "react-slick";
 
-import '../css/ProjectSlider.css';
-import "../Component Styles/ProjectSlider/ProjectSlider.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
-const ProjectSlider = () =>
-{
-  var settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1
-  };
+const ProjectSlider = () => {
+  // var settings = {
+  //   dots: true,
+  //   infinite: true,
+  //   speed: 500,
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1
+  // };
   return (
-    <div className="sliderContainer">
-      <Slider {...settings}>
-        {ProjectDetails.map((project) =>
-        {
-          const {
-            id,
-            title,
-            description,
-            images: { mainImage },
-            skills,
-          } = project;
+    // <Carousel {...settings}>
+    <Carousel>
+      {ProjectDetails.map((project) => {
+        const {
+          id,
+          title,
+          description,
+          images: { mainImage },
+          skills,
+        } = project;
 
-          return (
-            <div
-              key={id}
-              className="project__slide slick-list"
-              onClick={() => console.log("use history APi to change page")}
-            >
-              <Link to={`/Projects/Single_Project/${title}`}>
-                <div className="project__slideElementContent">
-                  <img src={mainImage} alt={title} />
-                  <div className="project__slideElementContentText">
-                    <h1>{title}</h1>
-                    <p> {description}</p>
-                  </div>
+        return (
+          <div
+            key={id}
+            className="project__slide"
+            onClick={() => console.log("use history APi to change page")}
+          >
+            <Link to={`/Projects/Single_Project/${title}`}>
+              <div className="project__slideElementContent">
+                <img src={mainImage} alt={title} />
+                <div className="project__slideElementContentText">
+                  <h1>{title}</h1>
+                  <p> {description}</p>
                 </div>
-              </Link>
-            </div>
-          );
-        })}
-      </Slider>
-    </div>
+              </div>
+            </Link>
+          </div>
+        );
+      })}
+    </Carousel>
   );
 };
 
