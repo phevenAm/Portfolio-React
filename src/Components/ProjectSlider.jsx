43d6b2390f8ee23041e922a3c5771aa529f0f2c2
Stephen Em <stephenmissah@outlook.com>
2022-06-26 15:55:43 +0100

    configuring slick

--- src/Components/ProjectSlider.jsx
+++ src/Components/ProjectSlider.jsx
@@ -1,64 +1,54 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import ProjectDetails from "../data/projects-data.json";
-// react-id-swiper
-// import "swiper/css/swiper.css";
-// import Swiper from "react-id-swiper";
-//customs css
-import '../css/ProjectSlider.css';
+import Slider from "react-slick";
 
+import '../css/ProjectSlider.css';
 import "../Component Styles/ProjectSlider/ProjectSlider.css";
 
 const ProjectSlider = () =>
 {
-  const params = {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    coverflowEffect: {
-      rotate: 40,
-      stretch: 0,
-      depth: 10, //100 original depth
-      modifier: 1,
-      slideShadows: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-    },
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
   };
-  // return (
-  //   // <Swiper {...params}>
-  //   //   {ProjectDetails.map((project) =>
-  //   //   {
-  //   //     const {
-  //   //       id,
-  //   //       title,
-  //   //       description,
-  //   //       images: { mainImage },
-  //   //       skills,
-  //   //     } = project;
+  return (
+    <div className="sliderContainer">
+      <Slider {...settings}>
+        {ProjectDetails.map((project) =>
+        {
+          const {
+            id,
+            title,
+            description,
+            images: { mainImage },
+            skills,
+          } = project;
 
-  //   //     return (
-  //   //       <div
-  //   //         key={id}
-  //   //         className="project__slide"
-  //   //         onClick={() => console.log("use history APi to change page")}
-  //   //       >
-  //   //         <Link to={`/Projects/Single_Project/${title}`}>
-  //   //           <div className="project__slideElementContent">
-  //   //             <img src={mainImage} alt={title} />
-  //   //             <div className="project__slideElementContentText">
-  //   //               <h1>{title}</h1>
-  //   //               <p> {description}</p>
-  //   //             </div>
-  //   //           </div>
-  //   //         </Link>
-  //   //       </div>
-  //   //     );
-  //   //   })}
-  //   // </Swiper>
-  // );
+          return (
+            <div
+              key={id}
+              className="project__slide slick-list"
+              onClick={() => console.log("use history APi to change page")}
+            >
+              <Link to={`/Projects/Single_Project/${title}`}>
+                <div className="project__slideElementContent">
+                  <img src={mainImage} alt={title} />
+                  <div className="project__slideElementContentText">
+                    <h1>{title}</h1>
+                    <p> {description}</p>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          );
+        })}
+      </Slider>
+    </div>
+  );
 };
 
 export default ProjectSlider;
