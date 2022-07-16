85a0e63af6489c0cbab822080b5b49171a31d0af
Stephen Em <stephenmissah@outlook.com>
2022-07-16 13:39:19 +0100

    Implemement my own carousel
    needs styling - check scss file for project slider

--- src/Components/ProjectSlider.jsx
+++ src/Components/ProjectSlider.jsx
@@ -1,51 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import ProjectDetails from "../data/projects-data.json";
-import Slider from "react-slick";
 
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
 
 const ProjectSlider = () => {
-  // var settings = {
-  //   dots: true,
-  //   infinite: true,
-  //   speed: 500,
-  //   slidesToShow: 1,
-  //   slidesToScroll: 1
-  // };
   return (
-    // <Carousel {...settings}>
-    <Carousel>
-      {ProjectDetails.map((project) => {
-        const {
-          id,
-          title,
-          description,
-          images: { mainImage },
-          skills,
-        } = project;
+    <section class="app">
+      <div class="slider-container">
 
-        return (
-          <div
-            key={id}
-            className="project__slide"
-            onClick={() => console.log("use history APi to change page")}
-          >
-            <Link to={`/Projects/Single_Project/${title}`}>
-              <div className="project__slideElementContent">
-                <img src={mainImage} alt={title} />
-                <div className="project__slideElementContentText">
-                  <h1>{title}</h1>
-                  <p> {description}</p>
+        {ProjectDetails.map((project) => {
+          const {
+            id,
+            title,
+            description,
+            images: { mainImage },
+            skills,
+          } = project;
+
+          return (
+            <div key={id}>
+              <Link to={`/Projects/Single_Project/${title}`}>
+                <div className="slide">
+                  <img src={mainImage} alt={title} />
+                  <div className="project__slideElementContentText">
+                    <h1>{title}</h1>
+                    <p> {description}</p>
+                  </div>
                 </div>
-              </div>
-            </Link>
-          </div>
-        );
-      })}
-    </Carousel>
-  );
-};
+              </Link>
+            </div>
+          )
+        })}
+
+      </div>
+    </section>
+  )
+}
+
 
 export default ProjectSlider;
